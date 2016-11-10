331815b5341fa8c8bb6497fe3806df80fa934475
shunge <gyishun@gmail.com>
2016-11-10 00:39:30 -0800

    Todo list in

---
+++ public/todo.js
@@ -0,0 +1,31 @@
+angular.module('todoApp', [])
+  .controller('TodoListController', function() {
+    var todoList = this;
+    todoList.todos = [
+      {text:'I spent 30 mins meditating today .', done:false},
+      {text:'Today I did 5 reality checks.', done:false},
+      {text:'Before I go to sleep, I told myself, <I am going to sleep.>', done:false},
+      {text:'I slept at least 6 hours today.', done:false},
+      {text:'I recored my drawing today.', done:false}];
+ 
+    todoList.addTodo = function() {
+      todoList.todos.push({text:todoList.todoText, done:false});
+      todoList.todoText = '';
+    };
+ 
+    todoList.remaining = function() {
+      var count = 0;
+      angular.forEach(todoList.todos, function(todo) {
+        count += todo.done ? 0 : 1;
+      });
+      return count;
+    };
+ 
+    todoList.archive = function() {
+      var oldTodos = todoList.todos;
+      todoList.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.done) todoList.todos.push(todo);
+      });
+    };
+  });
